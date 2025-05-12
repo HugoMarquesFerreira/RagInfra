97ad93e0fd660a4642fa40b4dacb9993ff3bc887
HugoMarquesFerreira <hugomarquesferreira@outlook.com>
2025-05-12 07:01:37 -0300

    first commit

---
+++ __main__.py
@@ -0,0 +1,204 @@
+import json
+import pulumi
+import pulumi_aws as aws
+import pulumi_aws_apigateway as apigateway
+from iam_roles import create_roles_and_policies
+import uuid
+
+# =======================
+# Configurações do Pulumi
+# =======================
+config = pulumi.Config('indecx-infra')
+
+# Configurações de Rede
+VPC_ID = config.require('vpcId')
+SUBNET_IDS = config.require_object('subnetIds')
+ALLOWED_SG_IDS = config.require('allowedSecurityGroupIds')
+
+# Configurações da Lambda
+lambda_config = config.require_object('lambdaConfig')
+LAMBDA_IMAGE_URI = lambda_config['imageUri']
+LAMBDA_TIMEOUT = lambda_config['timeout']
+LAMBDA_MEMORY_SIZE = lambda_config['memorySize']
+
+# Configurações do Qdrant
+qdrant_config = config.require_object('qdrantConfig')
+QDRANT_HOST = qdrant_config['host']
+QDRANT_API_KEY = qdrant_config['apiKey']
+
+# Configurações do API Gateway
+api_gw_config = config.require_object('apiGatewayConfig')
+API_NAME = api_gw_config['name']
+API_DESCRIPTION = api_gw_config['description']
+API_STAGE_NAME = api_gw_config['stageName']
+
+# ==============================
+# Security Group para a Lambda
+# ==============================
+lambda_sg = aws.ec2.SecurityGroup(
+    "lambdaSecurityGroup",
+    description="Security Group for Lambda to access internet via NAT",
+    vpc_id=VPC_ID,
+    egress=[{
+        "protocol": "-1",
+        "from_port": 0,
+        "to_port": 0,
+        "cidr_blocks": ["0.0.0.0/0"],
+    }]
+)
+
+# Regras de Ingress
+aws.ec2.SecurityGroupRule(
+    "lambdaAllowFromSGs",
+    type="ingress",
+    from_port=0,
+    to_port=0,
+    protocol="-1",
+    security_group_id=lambda_sg.id,
+    source_security_group_id=lambda_sg.id
+)
+
+aws.ec2.SecurityGroupRule(
+    "lambdaAllowFromExtraSG",
+    type="ingress",
+    from_port=0,
+    to_port=0,
+    protocol="-1",
+    security_group_id=lambda_sg.id,
+    source_security_group_id=ALLOWED_SG_IDS
+)
+
+# -----------------------
+# IAM: Papéis e Políticas
+# -----------------------
+validator_role = create_roles_and_policies()
+
+aws.iam.RolePolicyAttachment(
+    "lambda_basic_execution_job_scheduler",
+    role=validator_role.name,
+    policy_arn=aws.iam.ManagedPolicy.AWS_LAMBDA_BASIC_EXECUTION_ROLE,
+)
+
+# =======================
+# Função Lambda
+# =======================
+ALLOWED_SG_IDS = config.require_object('allowedSecurityGroupIds')
+
+for idx, sg_id in enumerate(ALLOWED_SG_IDS):
+    aws.ec2.SecurityGroupRule(
+        f"lambdaAllowFromExtraSG{idx}",
+        type="ingress",
+        from_port=0,
+        to_port=0,
+        protocol="-1",
+        security_group_id=lambda_sg.id,
+        source_security_group_id=sg_id
+    )
+
+lambda_rag = aws.lambda_.Function(
+    "ragFastApiLambda",
+    package_type="Image",
+    image_uri=LAMBDA_IMAGE_URI,
+    role=validator_role.arn,
+    timeout=LAMBDA_TIMEOUT,
+    memory_size=LAMBDA_MEMORY_SIZE,
+    environment={
+        "variables": {
+            "QDRANT_HOST": QDRANT_HOST,
+            "QDRANT_API_KEY": QDRANT_API_KEY,
+            "QDRANT_COLLECTION": ""
+        }
+    },
+    vpc_config={
+        "subnet_ids": SUBNET_IDS,
+        "security_group_ids": [lambda_sg.id] + ALLOWED_SG_IDS
+    },
+)
+
+# =======================
+# Permissões para API Gateway
+# =======================
+lambda_permission_api_gateway = aws.lambda_.Permission(
+    "allow_apigateway_to_invoke_lambda",
+    action="lambda:InvokeFunction",
+    function=lambda_rag.arn,
+    principal="apigateway.amazonaws.com",
+    source_arn=pulumi.Output.format("arn:aws:execute-api:{0}:{1}:*/*/*/*", 
+                                  aws.get_region().name,
+                                  aws.get_caller_identity().account_id)
+)
+
+# =======================
+# API Gateway
+# =======================
+api = aws.apigateway.RestApi(
+    "ragApi",
+    name=API_NAME,
+    description=API_DESCRIPTION,
+    endpoint_configuration={"types": "REGIONAL"}
+)
+
+proxy_resource = aws.apigateway.Resource(
+    "ProxyResource",
+    rest_api=api.id,
+    parent_id=api.root_resource_id,
+    path_part="{proxy+}"
+)
+
+proxy_method = aws.apigateway.Method(
+    "ProxyMethod",
+    rest_api=api.id,
+    resource_id=proxy_resource.id,
+    http_method="ANY",
+    authorization="NONE"
+)
+
+proxy_integration = aws.apigateway.Integration(
+    "LambdaProxyIntegration",
+    rest_api=api.id,
+    resource_id=proxy_resource.id,
+    http_method=proxy_method.http_method,
+    integration_http_method="POST",
+    type="AWS_PROXY",
+    uri=lambda_rag.invoke_arn,
+    opts=pulumi.ResourceOptions(depends_on=[proxy_method])
+)
+
+root_method = aws.apigateway.Method(
+    "RootMethod",
+    rest_api=api.id,
+    resource_id=api.root_resource_id,
+    http_method="ANY",
+    authorization="NONE"
+)
+
+root_integration = aws.apigateway.Integration(
+    "RootLambdaIntegration",
+    rest_api=api.id,
+    resource_id=api.root_resource_id,
+    http_method=root_method.http_method,
+    integration_http_method="POST",
+    type="AWS_PROXY",
+    uri=lambda_rag.invoke_arn,
+    opts=pulumi.ResourceOptions(depends_on=[root_method])
+)
+
+deployment = aws.apigateway.Deployment(
+    "ApiDeployment",
+    rest_api=api.id,
+    opts=pulumi.ResourceOptions(depends_on=[proxy_integration, root_integration])
+)
+
+stage = aws.apigateway.Stage(
+    "ProdStage",
+    rest_api=api.id,
+    deployment=deployment.id,
+    stage_name=API_STAGE_NAME
+)
+
+# =======================
+# Outputs
+# =======================
+pulumi.export("api_url", stage.invoke_url.apply(lambda url: f"{url}"))
+pulumi.export("lambda_arn", lambda_rag.arn)
+pulumi.export("lambda_sg_id", lambda_sg.id)
---
+++ iam_roles.py
@@ -0,0 +1,85 @@
+import json
+import pulumi
+import pulumi_aws as aws
+
+def create_roles_and_policies():
+    role_name = "validation-service-role"
+
+    validator_role = aws.iam.Role(
+        "validation-service-role",
+        name=role_name,
+        assume_role_policy=json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "lambda.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }),
+        # opts=pulumi.ResourceOptions(protect=True)
+    )
+
+    # Política associada ao Role
+    aws.iam.RolePolicy(
+        "image-validation-service-policy",
+        role=validator_role.id,
+        policy={
+            "Version": "2012-10-17",
+            "Statement": [
+                # Permissões gerais para Lambda e serviços utilizados
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "bedrock:*",
+                        "dynamodb:*",               
+                        "s3:*",                  # Leitura de arquivos no S3
+                        "cloudwatch:*",
+                        "secretsmanager:GetSecretValue", # Acesso a segredos
+                        "iam:PassRole",                 # Lambda assume o Role
+                        "iam:GetRole",
+                        "tag:GetResources",
+                        "lambda:*",
+                        "kinesis:*",
+                        "ec2:DescribeVpcs",
+                        "ec2:DescribeSubnets",
+                        "ec2:DescribeSecurityGroups",
+                        "ec2:CreateNetworkInterface",
+                        "ec2:DescribeNetworkInterfaces",
+                        "ec2:DeleteNetworkInterface"
+
+                    ],
+                    "Resource": "*"
+                },
+                # Permissão para criar grupos de logs no CloudWatch
+                {
+                    "Effect": "Allow",
+                    "Action": "logs:CreateLogGroup",
+                    "Resource": "arn:aws:logs:us-east-1:777825471459:*"
+                },
+                # Permissão para criar streams de log e enviar eventos
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "logs:CreateLogStream",
+                        "logs:PutLogEvents"
+                    ],
+                    "Resource": [
+                        "arn:aws:logs:us-east-1:777825471459:log-group:/aws/lambda/ValidationLambda:*"
+                    ]
+                },
+                # Permissão explícita para a Lambda assumir o Role
+                {
+                    "Effect": "Allow",
+                    "Action": "iam:PassRole",
+                    "Resource": validator_role.arn
+                }
+            ]
+        },
+        # opts=pulumi.ResourceOptions(protect=True)
+    )
+
+    return validator_role
